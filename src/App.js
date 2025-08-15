1627de2e8f0ddcbbd19031a1f70026431aa6efe7
Aditi Naik <aditinaik811@gmail.com>
2025-08-16 01:02:07 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import axios from 'axios'
+import aditi from '../src/assets/Aditi.png'
+import gemini from '../src/assets/gemini.webp'
+import SendIcon from '@mui/icons-material/Send';
+import MicIcon from '@mui/icons-material/Mic';
+import MicOffIcon from '@mui/icons-material/MicOff';
+import { useState } from 'react';
+import loader from '../src/assets/loader.gif';
+const API = process.env.REACT_APP_API_BASE_URL
 function App() {
+  const [question,setQuestion]=useState('');
+  const[ans,setAns] = useState('');
+  const[loading,setLoading]=useState(false)
+  const[speaking,setSpeaking]=useState(false)
+  const submitHandler = (e)=>{
+      e.preventDefault();
+      setLoading(true)
+      axios.post(`${API}/getResponse`,{
+        question:question
+      })
+      .then(result=>{
+        console.log(result)
+       setAns(result.data.response)
+       setLoading(false)
+      })
+      .catch(err=>{
+        console.log(err)
+        setLoading(false)
+      })
+      
+  }
+  const stopSpeakHandler = () => {
+    window.speechSynthesis.cancel();
+  }
+
+  const speakHandler = ()=>{
+      setSpeaking(true);
+      const a = new SpeechSynthesisUtterance(ans)
+      window.speechSynthesis.speak(a)
+  }
   return (
+    <>
+    <h1 className='heading'>Hii I am your AI Assistant</h1>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
+      <div className="box">
+        <div className='profile-pic'>
+            <img className="pic" src={aditi} alt="profile-pic"/>
+        </div>
+        <p className='label'>Question</p>
+        <textarea onChange={(e)=>{setQuestion(e.target.value)}}></textarea>
+        <button className="btn" onClick={submitHandler}>Send  <SendIcon/></button>
+      </div>
+      {loading &&
+      <div className='loader'>
+        <img src={loader}/>
+      </div>
+}
+      <div className="box">
+        <div className='profile-pic'>
+          
+            <img className="pic" src={gemini} alt="profile-pic"/>
+        </div>
+        <p className='label'>Response</p>
+        <textarea value={ans} className='response'></textarea>
+        <div className='response-btn'>
+        <button className='btn res-btn'  onClick={speakHandler}>Speak<MicIcon/></button>
+        {speaking &&<button className='btn res-btn' onClick={stopSpeakHandler}><MicOffIcon/>Stop</button>}
+        </div>
+      </div>
     </div>
+</>
   );
 }
 
